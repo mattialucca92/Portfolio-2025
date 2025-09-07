459503b12dee42250bf916d83f8eaa91f596e8d5
mattialucca92 <mattialucca92@gmail.com>
2025-09-07 18:24:22 +0200

    Fix vite config and add workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-2025/",
+  base: "/Portfolio-2025/",
   build: {
     outDir: "dist",
   },
