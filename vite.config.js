8854ccf32f1aa1ab0f8f7463663147d2b0820ff2
mattialucca92 <mattialucca92@gmail.com>
2025-09-07 18:37:56 +0200

    modifiche file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-2025/",
+  base: "/Portfolio-2025/", // Assicurati che sia esattamente così
   build: {
     outDir: "dist",
   },
